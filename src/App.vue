4f8cbe3e391ad0968511d91dcae88cd47462e918
song <634059453@qq.com>
2017-12-15 18:00:50 +0800

    dsadas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -18,6 +17,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ src/components/main.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-container>
+    <el-header style="padding: 0">
+
+    <el-row style="width: 100vw">
+      <el-col :span="24">
+        <img src="../assets/Website_Banner_Preview.png" style="width: 100%" alt="">
+      </el-col>
+    </el-row>
+    </el-header>
+
+    <el-main>
+
+    </el-main>
+
+  </el-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .main-container{
+    flex: 1200px 0 1;
+  }
+</style>
